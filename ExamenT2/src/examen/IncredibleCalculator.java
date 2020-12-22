c36ae10a3163b85f5253b640131d7037bd28eba1
Jaime Jiménez Ríos <jaimejimenezrios10@gmail.com>
2020-12-22 09:22:14 +0100

    Añadida división entera

--- ExamenT2/src/examen/IncredibleCalculator.java
+++ ExamenT2/src/examen/IncredibleCalculator.java
@@ -23,7 +23,11 @@ public class IncredibleCalculator {
                     break;
             case 3: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su producto.
                     break;
-            case 4: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división entera.
+            case 4:
+            	
+            	System.out.println("Aquí va la funcionalidad de la división entera");
+            	
+            	// Multiplicar. Debes leer dos números enteros de entrada e imprimir su división entera.
                     break;
             case 5: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división con decimales.
                     break;
